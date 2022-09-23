35c017cbc7f435ed31e701dad4079a570c322315
superwyh <superwyh@gmail.com>
2022-09-23 16:26:13 +0800

    3

--- 13.罗马数字转整数.cs
+++ 13.罗马数字转整数.cs
@@ -6,45 +6,45 @@
 // 暴力法，虽然看起来很蠢，但应该是效率最高的方法
 // 3999/3999 cases passed (68 ms)
 // Your runtime beats 68.79 % of csharp submissions
-// Your memory usage beats 78.55 % of csharp submissions (45.6 MB)
+// Your memory usage beats 93.52 % of csharp submissions (45.3 MB)
 // @lc code=start
 public class Solution {
     public int RomanToInt(string s) {
-     int result = 0;
+     int a = 0;
         for (int i = 0; i < s.Length; i++)
         {
             switch (s[i])
             {
                 case 'I':
-                    result += 1;
+                    a += 1;
                     if (i != s.Length - 1)
-                        if (s[i + 1] == 'V' || s[i + 1] == 'X') result -= 2;
+                        if (s[i + 1] == 'V' || s[i + 1] == 'X') a -= 2;
                     break;
                 case 'V':
-                    result += 5;
+                    a += 5;
                     break;
                  case 'X':
-                    result += 10;
+                    a += 10;
                     if (i != s.Length - 1)
-                        if (s[i + 1] == 'L' || s[i + 1] == 'C') result -= 20;
+                        if (s[i + 1] == 'L' || s[i + 1] == 'C') a -= 20;
                     break;
                 case 'L':
-                    result += 50;
+                    a += 50;
                     break;
                 case 'C':
-                    result += 100;
+                    a += 100;
                     if (i != s.Length - 1)
-                        if (s[i + 1] == 'D' || s[i + 1] == 'M') result -= 200;
+                        if (s[i + 1] == 'D' || s[i + 1] == 'M') a -= 200;
                     break;
                 case 'D':
-                    result += 500;
+                    a += 500;
                     break;
                 case 'M':
-                    result += 1000;
+                    a += 1000;
                     break;
             }
         }
-        return result;
+        return a;
         }
 }
 // @lc code=end
